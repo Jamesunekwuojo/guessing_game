4ca6c756a0a17ffb1d89714188676b0b718cb7c6
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-07-19 10:07:22 +0100

    cook ..

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,15 @@ fn main() {
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    println!("You guessed: {guess}")
+    println!("You guessed: {guess}");
+
+    let x = 5;
+    let y = 10;
+
+    println!("x= {x} and y + x = {} ", y +2);
 
     // By default in Rust variablrs are immutable for you to make it mutable, you need to introduce.. 'mut'
 
     // The :: synthax in the ::new line indicates  that new is an associated function of the String type.  An associated function is a function that’s implemented on a type, in this case String
-
-
 
 }
