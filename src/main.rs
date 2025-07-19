6bfe9cc61330fb77cf260af055aa61e9586c478d
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-07-19 07:32:46 +0100

    first part complted..

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use std:io;
+use std::io;
 
 fn main() {
     println!("Guess the number");
 
     println!("Please input your guess");
 
-    let mut guess = String::new() // it returns  a new instnace of a  string
+    let mut guess = String::new(); // it returns  a new instnace of a  string
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {guess}")
 
