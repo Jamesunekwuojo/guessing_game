090852fcc86f52b494adab0a5b154e977c25ed08
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-07-19 07:27:58 +0100

    first part of game completed..

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std:io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    println!("Please input your guess");
+
+    let mut guess = String::new() // it returns  a new instnace of a  string
+
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+    println!("You guessed: {guess}")
+
+    // By default in Rust variablrs are immutable for you to make it mutable, you need to introduce.. 'mut'
+
+    // The :: synthax in the ::new line indicates  that new is an associated function of the String type.  An associated function is a function that’s implemented on a type, in this case String
+
+
+
 }
